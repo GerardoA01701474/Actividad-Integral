8efac2ee33da906da46fb3f4c58c4f20948ada23
GerardoA01701474 <58478243+GerardoA01701474@users.noreply.github.com>
2020-09-19 21:33:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ string archivo = "archivo.txt";
 
 std::vector<int> numeros;
 std::vector<string> nombres;
-
+//-----------se abre el archivo---------------//
 ifstream leerArchivo (archivo);
 if (leerArchivo.is_open()){
   int count = 1;
   while (getline (leerArchivo, line) ){
     leerArchivo >> nombre >> venta;
-    cout << "nombre: " <<nombre << " venta: " << venta << '\n';
+    //cout << "nombre: " <<nombre << " venta: " << venta << '\n';
     numeros.resize(count,venta);
     nombres.resize(count, nombre);
     count++;
@@ -29,23 +29,22 @@ if (leerArchivo.is_open()){
 cout << "No se pudo abrir el archivo" << endl;
 }
 
-for (int i = 0; i < numeros.size(); i++){
-  cout << numeros[i] << " " << '\n';
-  cout << nombres[i] << " " << '\n';
-}
-///////777777777777777777777777777777777
-Clase<int> A;
-A.ordenaBurbuja(numeros);
-cout << A.busqBinaria(numeros, 8);
+// ------------Se crea un objeto y ordena los vectores----//
+Clase A;
+A.ordenaBurbuja(numeros, nombres);
 
+//---------------Se busca dentro del vector ordenado, recibe un rango---//
+A.busqSecuencial(numeros, nombres, 50, 2000);
+
+//------------El vector ordenado se escribe en un archivo nuevo
 ofstream myfilein ("ejemplo.txt");
 if (myfilein.is_open()){
   for (int i = 0; i < numeros.size(); i++){
-    myfilein << numeros[i] << '\n';
-    myfilein << nombres[i] << '\n';
+    myfilein << numeros[i] << ' ';
+    myfilein << nombres[i] << ' ' << '\n';
   }
   myfilein.close();
-}else{
+}else{ 
   cout << "No se puede abrir el archivo";
 }
 return 0; 
