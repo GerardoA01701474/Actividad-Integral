8575338105b5d119e23476ca7cdd20141c8d07aa
GerardoA01701474 <58478243+GerardoA01701474@users.noreply.github.com>
2020-10-07 14:27:16 -0500

    Update main.cpp

--- Proyecto2/main.cpp
+++ Proyecto2/main.cpp
@@ -3,30 +3,25 @@
 #include <vector>
 #include <list>
 #include "list.h"
+//#include "match.h"
 using namespace std;
 
 int main(){
 string line;
 string nombre;
 int venta;
-string archivo = "archivo.txt";
+string archivo = "archivo.txt";//nombre del archivo
 
+//se crea un objeto de tipo lista
+List A;
 
-List<int> A;
-
-//std::vector<int> numeros;
-std::vector<string> nombres;
 //-----------se abre el archivo---------------//
 ifstream leerArchivo (archivo);
 if (leerArchivo.is_open()){
   int count = 1;
   while (getline (leerArchivo, line) ){
     leerArchivo >> nombre >> venta;
-    //cout << "nombre: " <<nombre << " venta: " << venta << '\n';
-    //numeros.resize(count,venta);
-    nombres.resize(count, nombre);
-
-    A.add(venta);
+    A.add(nombre,venta);//vamos registrando en la lista A
     count++;
   }
   leerArchivo.close();
@@ -34,7 +29,11 @@ if (leerArchivo.is_open()){
 cout << "No se pudo abrir el archivo" << endl;
 }
 
+//la función find recibe un rango de busqueda(min,max)
+/*esta función devuelve el resultado de la busqueda 
+en la consola*/
 A.find(18,19);
+//limpiamos la lista al finalizar
 A.clear();
 
   return 0;
