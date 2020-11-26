ff826da40905ffe8f1d64146d155e9ae06d6fd4e
GerardoA01701474 <58478243+GerardoA01701474@users.noreply.github.com>
2020-11-25 22:15:34 -0600

    Update main.cpp

--- Proyecto1/main.cpp
+++ Proyecto1/main.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include "clase.h"
+using namespace std;
 
+int main () {
+string line;
+string nombre;
+int venta;
+string archivo = "archivo.txt";
+
+std::vector<int> numeros;
+std::vector<string> nombres;
+//-----------se abre el archivo---------------//
+ifstream leerArchivo (archivo);
+if (leerArchivo.is_open()){
+  int count = 1;
+  while (getline (leerArchivo, line) ){
+    leerArchivo >> nombre >> venta;
+    //cout << "nombre: " <<nombre << " venta: " << venta << '\n';
+    numeros.resize(count,venta);
+    nombres.resize(count, nombre);
+    count++;
+  }
+  leerArchivo.close();
+}else{
+cout << "No se pudo abrir el archivo" << endl;
+}
+
+// ------------Se crea un objeto y ordena los vectores----//
+Clase A;
+A.ordenaBurbuja(numeros, nombres);
+
+//---------------Se busca dentro del vector ordenado, recibe un rango---//
+A.busqSecuencial(numeros, nombres, 50, 2000);
+
+//------------El vector ordenado se escribe en un archivo nuevo
+ofstream myfilein ("ejemplo.txt");
+if (myfilein.is_open()){
+  for (int i = 0; i < numeros.size(); i++){
+    myfilein << numeros[i] << ' ';
+    myfilein << nombres[i] << ' ' << '\n';
+  }
+  myfilein.close();
+}else{ 
+  cout << "No se puede abrir el archivo";
+}
+return 0; 
+}  
