a813c7b23d35d82263d3dc0300f3c0e21de2be51
GerardoA01701474 <58478243+GerardoA01701474@users.noreply.github.com>
2020-12-01 23:48:54 -0600

    Update main.cpp

--- Proyecto2/main.cpp
+++ Proyecto2/main.cpp
@@ -7,20 +7,39 @@
 using namespace std;
 
 int main(){
-
+int op;
 //se crea un objeto de tipo lista
 List A;
 A.leer();
+while (op != 3){
+  cout << "¿Qué quieres hacer: "<<'\n';
+  cout << "1.- agregar nueva edad"<<'\n';
+  cout << "2.- Filtrar las edades por rango"<<'\n';
+  cout << "3.- salir/terminar programa" << '\n';
+  cin >> op;
+  if (op == 1){
+    int edad;
+    string nombre;
+    cout << "ingresa el nombre: ";
+    cin >> nombre;
+    cout << "ingresa la edad: ";
+    cin >> edad;
+    A.add(nombre,edad);
+  }
+  if (op == 2){
+    int min, max;
+    cout << "Limite inferior de edades: ";
+    cin >> min;
+    cout << "Limite superior de edades: ";
+    cin >> max;
+    A.find(min,max);
+  }
+}
 
 //la función find recibe un rango de busqueda(min,max)
 /*esta función devuelve el resultado de la busqueda 
 en la consola*/
-int min, max;
-cout << "Limite inferior de edades: ";
-cin >> min;
-cout << "Limite superior de edades: ";
-cin >> max;
-A.find(min,max);
+
 //limpiamos la lista al finalizar
 A.clear();
 
